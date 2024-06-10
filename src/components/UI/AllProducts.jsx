eab16682c25bd4421d3e7cb959dde7734582b87f
Afsana Meem <afsumeem@gmail.com>
2024-06-10 21:24:23 +0600

    all product section done

--- src/components/UI/AllProducts.jsx
+++ src/components/UI/AllProducts.jsx
@@ -1,25 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { TbCurrencyTaka } from "react-icons/tb";
 
 const AllProducts = ({ products }) => {
   return (
     <div className="container mx-auto mt-14">
       <h2 className="text-center text-2xl font-bold mb-5">All Products</h2>
+
+      {/* all products */}
       <div className=" gap-6 grid grid-cols-12 grid-rows-2 px-8">
         {products.map((product, i) => (
           <div
-            className="col-span-12 md:col-span-6 lg:col-span-3  m-2 p-7 border gap-5"
+            className="col-span-12 md:col-span-6 lg:col-span-3  m-2 p-7 border-none gap-5 relative shadow-lg"
             key={i}
           >
             <img
               alt="Card background"
-              className="object-cover rounded-xl mb-4 w-full h-40"
+              className="object-cover rounded-xl mb-4 w-full h-32 my-5"
               src={product.image}
               // width={270}
             />
             <h4 className="font-semibold text-sm mt-3 ">
               {product.productName}
             </h4>
+            <p className="mt-2">Model: {product?.model}</p>
+            <p className="flex items-center gap-1 mt-5">
+              <TbCurrencyTaka className="text-xl" />{" "}
+              <span className="text-sm font-semibold">{product.price}</span>
+            </p>
+            <p className="bg-blue-500 p-1 text-white w-fit text-xs absolute top-0 right-0">
+              {product?.category}
+            </p>
           </div>
         ))}
       </div>
