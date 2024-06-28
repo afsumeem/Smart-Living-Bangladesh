782f1f370eb4aa663baae737c6fc2511e5f355b9
Afsana Meem <afsumeem@gmail.com>
2024-06-29 00:59:54 +0600

    slide updated

--- src/components/UI/Banner.jsx
+++ src/components/UI/Banner.jsx
@@ -28,7 +28,49 @@ const Banner = () => {
 
             <div className="shadow-2xl rounded-2xl bg-black   relative ">
               <div
-                className="overflow-hidden  bg-cover w-full h-[620px] rounded-2xl banner-bg"
+                className="overflow-hidden  bg-cover w-full h-[600px] rounded-2xl banner-bg"
+                // style={{
+                //   background:
+                //     "linear-gradient(90deg, rgba(0,53,148,1) 1%, rgba(115,180,208,1) 97%)",
+
+                // }}
+              >
+                {" "}
+                <p className="absolute left-16 bottom-36 text-6xl text-white font-light">
+                  Smart Homes for <br /> Smarter Bangladesh
+                </p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="background-div bg-cover w-[100%] mx-auto grid grid-cols-1 gap-5 rounded-2xl">
+            {/* content */}
+
+            <div className="shadow-2xl rounded-2xl bg-black   relative ">
+              <div
+                className="overflow-hidden  bg-cover w-full h-[600px] rounded-2xl banner-bg"
+                // style={{
+                //   background:
+                //     "linear-gradient(90deg, rgba(0,53,148,1) 1%, rgba(115,180,208,1) 97%)",
+
+                // }}
+              >
+                {" "}
+                <p className="absolute left-16 bottom-36 text-6xl text-white font-light">
+                  Smart Homes for <br /> Smarter Bangladesh
+                </p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="background-div bg-cover w-[100%] mx-auto grid grid-cols-1 gap-5 rounded-2xl">
+            {/* content */}
+
+            <div className="shadow-2xl rounded-2xl bg-black   relative ">
+              <div
+                className="overflow-hidden  bg-cover w-full h-[600px] rounded-2xl banner-bg"
                 // style={{
                 //   background:
                 //     "linear-gradient(90deg, rgba(0,53,148,1) 1%, rgba(115,180,208,1) 97%)",
